3a3649e0c210365ed013c9c10c1c51aa0f77b472
Hemanth <hemanthkovuruk3@gmail.com>
2021-03-14 00:12:14 +0530

    initial commit

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use("/api/v1/rides", rideRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
-  app.all("/*", (req, res) =>
+  app.get("/", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 }
